1c2cfc49a840760967dd371a5f1d2075a37a79ce
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-01-12 04:11:34 +0530

    header and footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <Fragment>
-      <h1>Hello</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>eCommerce</h1>
+        </Container>
+      </main>
+      <Footer />
     </Fragment>
   );
 }
