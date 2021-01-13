7cc94309a282c1e3d4ab3bd23a8ee0e4eee7dd25
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-01-13 17:39:34 +0530

    cart screen

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -3,19 +3,21 @@ import { CART_ADD_ITEM } from '../constants/cartConstants';
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
         case CART_ADD_ITEM:
-            const item = action.payload;
+            const item = action.payload
 
-            const existItem = state.cartItems.find(x => x.product === item.product)
+            const existItem = state.cartItems.find((x) => x.product === item.product)
 
-            if(existItem){
+            if (existItem) {
                 return {
-                    ...state,
-                    cartItems: state.cartItems.map(x => x.product === existItem.item.product ? item : x)
+                ...state,
+                cartItems: state.cartItems.map((x) =>
+                    x.product === existItem.product ? item : x
+                ),
                 }
-            } else{
+            } else {
                 return {
-                    ...state,
-                    cartItems: [...state.cartItems, item]
+                ...state,
+                cartItems: [...state.cartItems, item],
                 }
             }
     
