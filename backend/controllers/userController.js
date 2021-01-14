57721ce8109bf9aa7e3b307a04ef08ea0f4fd6a2
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-01-14 10:52:37 +0530

    user registration and password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,40 @@ const asyncHandler = require('express-async-handler');
 const generateToken = require('../utils/generateToken');
 const User = require('../models/userModel');
 
+// @route     POST api/users
+// @desc      regsiter a new user
+// @access    public
+const registerUser = asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email: email })
+
+    
+    if(userExists){
+        res.status(400);
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else{
+        res.status(400);
+        throw new Error('Invalid user data')
+    }
+})
+
 // @route     POST api/users/login
 // @desc      auth user and get token
 // @access    public
@@ -45,5 +79,6 @@ const getUserProfile = asyncHandler(async(req, res) => {
 
 module.exports = {
     authUser: authUser,
-    getUserProfile: getUserProfile
+    getUserProfile: getUserProfile,
+    registerUser: registerUser
 };
