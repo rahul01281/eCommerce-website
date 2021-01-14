2979b40c685c0fe59cdb518cb153d3dee517132e
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-01-14 09:32:35 +0530

    custom authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,26 @@ const authUser = asyncHandler(async(req, res) => {
     }
 })
 
+// @route     GET api/users/profile
+// @desc      get user profile
+// @access    private
+const getUserProfile = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else{
+        res.status(404);
+        throw new Error('User not found')
+    }
+})
+
 module.exports = {
-    authUser: authUser
+    authUser: authUser,
+    getUserProfile: getUserProfile
 };
